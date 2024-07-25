fdfb73bea43121e04df5dfdf9fdbafa2b9bf36b2
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-07-25 20:59:57 +0530

    made sticky navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className='bg-gradient-to-r from-cyan-500 to-blue-500 shadow-md'>
+    <nav className='bg-gradient-to-r from-cyan-500 to-blue-500 shadow-md sticky top-0 z-50'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex justify-between items-center h-16'>
           <div className='flex items-center'>
