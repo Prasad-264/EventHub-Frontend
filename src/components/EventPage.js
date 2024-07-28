6a50b88bcb414a928e6c21920c2fb1e394d58b5f
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-07-28 19:11:48 +0530

    Update participants

--- src/components/EventPage.js
+++ src/components/EventPage.js
@@ -52,13 +52,13 @@ const EventPage = () => {
   return (
     <div className='flex flex-wrap flex-row gap-4 justify-evenly p-4'>
       <div className="max-w-2xl bg-white p-6 rounded-xl shadow-md">
-        <h1 className="text-3xl font-bold mb-4">{event.title}</h1>
-        <img className="w-full h-64 object-cover mb-4 rounded" src={event.image} alt={event.title} />
+        <h1 className="text-3xl font-bold mb-4">{event?.title}</h1>
+        <img className="w-full h-64 object-cover mb-4 rounded" src={event?.image} alt={event?.title} />
         <div className='flex justify-between mb-4'>
-          <p className="text-gray-600">{event.location}</p>
+          <p className="text-gray-600">{event?.location}</p>
           <p className="text-gray-600">{formattedDate}</p>
         </div>
-        <p className="text-gray-700 mb-4">{event.description}</p>
+        <p className="text-gray-700 mb-4">{event?.description}</p>
         <button
           onClick={handleRegister}
           className="text-white py-2 px-4 w-full rounded bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-cyan-600 hover:to-blue-600 transition duration-300"
@@ -69,26 +69,17 @@ const EventPage = () => {
       <div className="max-w-2xl bg-white p-6 rounded-xl shadow-md text-center">
         <h1 className="text-3xl font-bold mb-4">Participants</h1>
         <div className="max-h-[29rem] overflow-y-auto no-scrollbar">
-          {/* {event.participants?.map((participant, index) => (
-            <div key={index} className='flex justify-between mb-4'>
-              <p>{participant.name}</p>
+          {event.participants?.map((participant, index) => (
+            <div key={index} className='flex justify-between items-center gap-3 mb-4 p-4 bg-white shadow-md rounded-lg'>
+              <p className='text-lg font-medium text-gray-800'>{participant.firstName} {participant.lastName}</p>
               <button
-                onClick={() => handleAddFriend(participant.id)}
-                className="bg-gray-300 text-gray-700 py-1 px-2 rounded hover:bg-gray-400 transition duration-300"
+                onClick={() => handleAddFriend(participant._id)}
+                className="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600 transition duration-300"
               >
                 Add Friend
               </button>
-            </div>
-          ))} */}
-          <div className='flex justify-between items-center gap-3 mb-4 p-4 bg-white shadow-md rounded-lg'>
-            <p className='text-lg font-medium text-gray-800'>Prasad Kandekar</p>
-            <button
-              onClick={() => handleAddFriend()}
-              className="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600 transition duration-300"
-            >
-              Add Friend
-            </button>
-          </div>          
+            </div> 
+          ))} 
         </div>
       </div>
     </div>
