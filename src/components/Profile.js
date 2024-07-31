5e7487d61bb43fdb0ced462cf824489baddaa89a
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-07-31 22:26:44 +0530

    Modified Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { getData } from '../utils/storage';
+import RegisteredEvents from './RegisteredEvents';
+// import Interests from './Interests';
 
 const Profile = () => {
+  const userName = getData('firstName');
+  const userId = getData('userId');
+  const token = getData('token');
+
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="mx-auto mt-10">
+      <div className="max-w-4xl mx-auto bg-white p-4 rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold text-center">Welcome, {userName}!</h1>
+      </div>
+      <RegisteredEvents userId={userId} token={token} />
+    </div>
+  );
+};
 
 export default Profile;
